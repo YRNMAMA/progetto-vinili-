c805b050b6a9cf9b298a5d9b8922302751a7d94c
Gningue Mamadou <91191658+YRNMAMA@users.noreply.github.com>
2023-04-22 20:54:14 +0000

    PRIMA DEL DISASTRO

--- my-remix-app/app/root.tsx
+++ my-remix-app/app/root.tsx
@@ -1,36 +1,10 @@
-import type { LinksFunction } from "@remix-run/node"; // or cloudflare/deno
+import Toolbar from "./shared/components/Toolbar";
 
-
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-import stylesheet from "~/tailwind.css";
-
-
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
-];
-export default function App() {
+export function Layout({ children }: any) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links/>
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    <>
+      <Toolbar />
+      {children}
+    </>
+  )
 }
--- my-remix-app/app/routes/_index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Index() {
-  return (
-    <div className="h-screen w-full bg-slate-600">
-      <h2 className="font-bold text-5xl text-red-400" > Vicene was here and trolled you!</h2>
-<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.F-zsvjAH55B5SQ9hYjuqzwHaHt%26pid%3DApi&f=1&ipt=7bc68e7b9feec58b9cc48e90177bebdeea254c69edc09e2d7645efe19579191d&ipo=images" alt="" />
-    </div>
-  );
-}
-  //15 e 28 problemi con il css//
